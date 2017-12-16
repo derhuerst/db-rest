fcc996837d7a2c1410cec033bd1c74ecad49b7cd
Jannis R <mail@jannisr.de>
2017-12-16 09:06:35 +0100

    use hafas-rest-api

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 'use strict'
 
-const http = require('http')
+const hafas = require('hafas-client')
+const dbProfile = require('hafas-client/p/db')
+const createApi = require('hafas-rest-api')
+const createLogging = require('hafas-rest-api/logging')
+const hsts = require('hsts')
 
-const api = require('./api')
-const server = http.createServer(api)
+const pkg = require('./package.json')
+const stations = require('./lib/stations')
+const allStations = require('./lib/all-stations')
 
-const port = process.env.PORT || 3000
-const hostname = process.env.HOSTNAME || ''
+const config = {
+	hostname: process.env.HOSTNAME || '1.db.transport.rest',
+	port: process.env.PORT || 3000,
+	name: pkg.name,
+	homepage: pkg.homepage
+}
 
-server.listen(port, (err) => {
+const client = hafas(dbProfile)
+const api = createApi(client, config)
+
+api.use(createLogging())
+
+api.get('/stations', stations)
+api.get('/stations/all', allStations)
+
+module.exports = api
+
+api.listen(config.port, (err) => {
 	if (err) {
 		console.error(err)
-		process.exit(1)
+		process.exitCode = 1
 	} else {
-		console.info(`Listening on ${hostname}:${port}.`)
+		console.info(`Listening on ${config.hostname}:${config.port}.`)
 	}
 })
